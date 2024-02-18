eb07a8f7f250f35c08145571b215f9307b8fd37f
Johan2511 <johanpenuelah@gmail.com>
2024-02-17 19:16:27 -0500

    Creación de hook personalizado para el modal y darle la funcionalidad al modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,17 +1,14 @@
-import {useState} from 'react';
 import Formulario from '../Formulario/Formulario';
 import './Modal.css';
 
-const Modal = ({ children }) => {
-
-  const [isOpem, setIsOpen] = useState(false)
+const Modal = ({ children, isOpen, closeModal }) => {
 
   return (
-    <article className='modal is-open'>
+    <article className={`modal ${isOpen && "is-open"}`}>
       <div className='modal-container'>
-        <div className='modal-close'>X</div>
+        <div className='modal-close' onClick={closeModal}>X</div>
         {children}
-        <Formulario />
+        <Formulario isOpen={isOpen} />
       </div>
     </article>
   );
